3ca3b5fabfa8c74a19cebb7a1eca847f4e99aa03
Maria <mk4036@columbia.edu>
2020-03-30 14:09:00 -0400

    reallocating based on type with a safe write func

--- realloc.c
+++ realloc.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define BASESIZE 100
 #define BOUNDARY .66
-#define REALLOC 2
+#define REALLOC  2
+
+#define VOID    0
+#define STR     1
+#define INT     2
+#define DOUBLE  3       
+#define FLOAT   4
 
 struct Heap
 {
@@ -26,23 +33,33 @@ void __contructor(struct Heap *id)
 	__malloc(id);       
 	id->written = 0;
 	id->size = BASESIZE;
-	id->type = 0;
+	id->type = VOID;
 
 }
 void __destructor(struct Heap *id)
 {
-
 	switch(id->type)
 	{
-		case 1: free((char *)id->addr);
-	}	
+		case STR: 
+			free((char *)id->addr);
+			break;
 
+		case INT:
+			free((int *)id->addr);
+			break;
 
+		case DOUBLE:
+			free((double *)id->addr);
+			break;
+
+		case FLOAT:
+			free((float *)id->addr);
+			break;
+	}
 }
 void __realloc(struct Heap *id)
 {
 	int size = id->size;
-
 	id->addr = realloc(id->addr, size*REALLOC);
 	id->size = size*REALLOC;
 }
@@ -63,75 +80,93 @@ int __boundary(struct Heap *id)
 
 void __safe_write(struct Heap *id, int wsize, void *val)
 {
-
 	__realloc_exact(id, wsize);
 } 
 
-void safe_write(struct Heap *id, void* val, int type)
+void safe_write(struct Heap *id, void *val, int type)
 {
-
 	switch(type) {
-		case 1:
+		case STR:
 			__safe_write(id, (strlen((char *)val) + 1), val);
 			strcpy(id->addr, ((char *)val));
-			id->type = 1;
-			/*        case 2:
-				  __safe_write(id, sizeof(char), val);
-			 *(char *)(id->addr) = ((char)val);
-			 case 3:
-			 __safe_write(id, sizeof(int), val);
-			 *(int *)(id->addr) = ((int )val);
-			 case 4:
-			 __safe_write(id, sizeof(double), val);
-			 *(double *)(id->addr) = ((double)val);
-			 case 5:
-			 __safe_write(id, sizeof(float), val);
-			 *(float *)(id->addr) = ((float)val);
-			 */   
-	}
+			id->type = STR;
+			break;
 
+		case INT:
+			__safe_write(id, sizeof(int), val);
+			*(int *)(id->addr) = *((int *)val);
+			id->type = INT;
+			break;
+
+		case DOUBLE:
+			__safe_write(id, sizeof(double), val);
+			*(double *)(id->addr) = *((double *)val);
+			id->type = DOUBLE;
+			break;
+
+		case FLOAT:
+			__safe_write(id, sizeof(float), val);
+			*(float *)(id->addr) = *((float *)val);
+			id->type = FLOAT;
+			break;
+	}
 }
 
 void str_safe_write(struct Heap *id, char *val)
 {
-	safe_write(id, (void *)val, 1);
+	safe_write(id, (void *)val, STR);
 }
-/*
-   void char_safe_write(struct Heap *id, void val)
-   {
-   safe_write(id, val, 2);
-   }
 
-   void int_safe_write(struct Heap *id, void val)
-   {
-   safe_write(id, val, 3);
-   }
+void int_safe_write(struct Heap *id, int val)
+{
+	safe_write(id, &val, INT);
+}
 
-   void float_safe_write(struct Heap *id, void val)
-   {
-   safe_write(id, val, 5);
-   }
+void float_safe_write(struct Heap *id, float val)
+{
+	safe_write(id, &val, FLOAT);
+}
 
-   void double_safe_write(struct Heap *id, void val)
-   {
-   safe_write(id, val, 6);
-   }
-   */
+void double_safe_write(struct Heap *id, double val)
+{
+	safe_write(id, &val, DOUBLE);
+}
+
 int main()
 {
-
 	struct Heap h1;
 	struct Heap h2;
 	struct Heap h3;
 	struct Heap h4;
-	struct Heap h5;
 
+	double test;
+
+	printf("Allocating memory...");
 	__contructor(&h1);
+	__contructor(&h2);
+	__contructor(&h3);
+	__contructor(&h4);
 
-	str_safe_write(&h1, "hello!!!!");
-	printf("%s", (char *)h1.addr);
+	printf("Testing writing and overwriting char * in heap...\n");
+	str_safe_write(&h1, "Clean C");
+	printf("%s\n", (char *)h1.addr);
+	str_safe_write(&h1, "Kleen C");
+	printf("%s\n", (char *)h1.addr);
+	printf("SUCCESS IN OVERWRITING CHAR *\n");
 
-	__destructor(&h1);
+	printf("Testing storing ints, floats, doubles in heap...\n");
+	int_safe_write(&h2, 10);
+	double_safe_write(&h3, 10.1);
+	double_safe_write(&h4, 10.2);
+	test = *(double *)(h3.addr) + *(double *)(h4.addr);
+	printf("SUCCESS IN ADDITION, TEST = %f\n", test);
+
+	printf("Deallocating memory...");
+	__destructor(&h1);    
+	__destructor(&h2);
+	__destructor(&h3);
+	__destructor(&h4);
+
 	return 0;
 }
 
