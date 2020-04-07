3b400aac519e7481be1b048fa3c09aa547db250c
Julia Kalimova <juliakalimova@Julias-MacBook-Pro-2.local>
2020-04-07 15:33:56 +0300

    array access for strings. Need to add error&sanity/correctness checks

--- arrayaccess.c
+++ arrayaccess.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include <assert.h>
 
+/* QUESTION: do we want this functionality for arrays of ANY type? */
+
 /* if I understand correctly,
 we would be calling this C code
 from our kleenc.ml compiler,
 so we would probably be pattern matching
 on various array access syntax variations, i.e.:
-arr[int a: int b] ->
+arr[int a: int b] -> subarray
 arr[:int b] -> end_index_array
 Arr[a:] -> start_index_array
 Arr[:] -> whole_array
@@ -45,20 +47,31 @@ char *end_index_array(char *array, int end)
 	return subarray;
 }
 
+char *subarray(char *array, int start, int end)
+{
+	char *subarray = malloc(end - start + 1);
+	
+	for (int i = start; i < end + 1; i++) {
+		subarray[i - start] = array[i];
+	}
+	
+	return subarray;
+}
+
 int main()
 {
 	char *string = "test string";
-/*
-	char *new_string = malloc(strlen(string) + 1);
 	
-	strcpy(new_string, string+5);
-*/
 	char *start_string = start_index_array(string, 5);
 	char *end_string = end_index_array(string, 3);
+	char *whole_string = whole_array(string);
+	char *substring = subarray(string, 5, 7);
 
 	printf("Original String: %s\n", string);
 	printf("New String: %s\n", start_string);
 	printf("New String: %s\n", end_string);
+	printf("Whole String: %s\n", whole_string);
+	printf("SubString: %s\n", substring);
 
 	return 0;
 }
