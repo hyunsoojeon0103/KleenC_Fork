1f11c44b489e836f34e6f3e4c5139a399bd2b82e
hyun <hyunsoojeon0103@gmail.com>
2020-04-09 00:11:11 -0400

    stdlib abs imported

--- funcs.c
+++ funcs.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-int getAbs(int x)
-{
-    return (x < 0)? -x : x;
-}
-*/
 int getPow(int x, int p)
 {
     int i;
@@ -18,7 +12,6 @@ int getPow(int x, int p)
 int main(int argc, char** args)
 {
     int x = 10;
-    fprintf(stdout,"%d\n",getAbs(x));
     fprintf(stdout,"%d\n",getPow(x,2));
 
     return 0;
